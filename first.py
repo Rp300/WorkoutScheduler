f04677326c96e2ac72ddcaa0d247a1eeacdfff36
rbalaji3 <rishikeshbalaji@wirelessprv-10-195-182-29.near.illinois.edu>
2018-10-12 18:54:50 -0500

    basics

--- first.py
+++ first.py
@@ -2,25 +2,48 @@ class User:
     myCourses = None
     myBodyType = None
     myWorkoutSpecs = None
-    def __init__(courses, bodyType, workoutSpecs):
-        schedule = courses.get
-
+    def __init__(self, courses, bodyType, workoutSpecs):
+        self.myCourses = courses
+        self.myBodyType = bodyType
+        self.myWorkoutSpecs = workoutSpecs
 class schedule:
     courseList = []
-    def __init__(cList):
-        courseList = cList
+    def __init__(self,cList):
+        self.courseList = cList
     
-    def addCourse():
-
-    def removeCourse():
+    def addCourse(self,c):
+        self.courseList.append(c)
+    
+    def removeCourse(self, c):
+        self.courseList.remove(c)
 
 
 class bodyType:
+    height = 0
+    weight = 0
+    BMI = 0
+
+    def __init__(self, h, w, bmi):
+        self.height = h
+        self.weight = w
+        self.BMI = bmi
 
 class workoutSpecs:
+    benchMax = 0
+    squatMax = 0
+    deadliftMax = 0
 
-class Krithi:
-    print("i have a big schlong")
+    def __init__(self, bM, sM, dM):
+        self.benchMax = bM
+        self.squatMax = sM
+        self.deadliftMax = dM
 
-p1 = User()
-print(p1.x)
+class course:
+    courseName = ""
+    courseStartTime = 0
+    courseEndTime = 0
+    
+    def __init__(self, name, startTime, endTime):
+        self.courseName = name
+        self.courseStartTime = startTime
+        self.courseEndTime = endTime
