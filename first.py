a6313d03919792af0628b97c9fdd1ec4035623d1
rbalaji3 <rishikeshbalaji@wirelessprv-10-195-182-29.near.illinois.edu>
2018-10-12 18:34:42 -0500

    started framework

--- first.py
+++ first.py
@@ -1,5 +1,25 @@
 class User:
-    x = 5
+    myCourses = None
+    myBodyType = None
+    myWorkoutSpecs = None
+    def __init__(courses, bodyType, workoutSpecs):
+        schedule = courses.get
+
+class schedule:
+    courseList = []
+    def __init__(cList):
+        courseList = cList
+    
+    def addCourse():
+
+    def removeCourse():
+
+
+class bodyType:
+
+class workoutSpecs:
+
+class
 
 p1 = User()
 print(p1.x)
